49bfe6ad42b1e08a66aaca157b582fbe680eb53c
lianaschapiro <liana.schapiro@gmail.com>
2016-02-23 13:19:48 -0500

    CSS stylings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,20 +15,10 @@
 //= require turbolinks
 //= require_tree .
 
-// function getCountryLocation(){
-// 	map.setCenter(1.0667,1.8833)
-// }
 
-// function initialize() {
-  
-//   var mapProp = {
-//     center:new google.maps.LatLng(lat, lng),
-//     zoom:7,
-//     mapTypeId:google.maps.MapTypeId.ROADMAP
-//   };
-//   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-// }
-// google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
 
 
 function openBoardpostModal() {
