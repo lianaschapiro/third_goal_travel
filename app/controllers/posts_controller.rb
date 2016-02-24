2d1442a8052d586616a55b593c8ab2993e8f393e
lianaschapiro <liana.schapiro@gmail.com>
2016-02-23 19:04:47 -0500

    Added pins on the map

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,17 +3,31 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
   end
 
   def create
+    @country = Country.find_by_url_name(params[:country_id])
+    @post = @country.posts.build(post_params)
+    # coordinates = Geocoder.coordinates(@post.address)
+    # @post.latitude = coordinates[0]
+    # @post.longitude = coordinates[1]
+    @post.save
+    redirect_to country_path(@country)
   end
 
   def edit
   end
 
   def update
+  end
+
+
+  private
+  def post_params
+      params.require(:post).permit(:title, :body, :latitude, :longitude, :address, :full_address, :user_id, :country_id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :country
   has_many :comments, dependent: :destroy
+
+ #  	geocoded_by :address
+	# reverse_geocoded_by :latitude, :longitude, :address => :full_address
+
+	# after_validation :geocode, :reverse_geocode
+
 end
