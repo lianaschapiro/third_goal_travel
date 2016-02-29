cb671b1b6b65d721ecae1b266ec7186f3b7c2cd5
lianaschapiro <liana.schapiro@gmail.com>
2016-02-28 19:57:17 -0500

    Added country ID to journals

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :review
+
+  validates_presence_of :body, :user_id, :review_id
 end
--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,3 +1,6 @@
 class Journal < ActiveRecord::Base
   belongs_to :country
+  belongs_to :user
+
+  validates_presence_of :title, :link, :user_id, :country_id
 end
